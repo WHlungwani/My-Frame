3232897678d3e1bf346784e613c9e184e6ab81eb
WELCOME HLUNGWANI <139961202+WHlungwani@users.noreply.github.com>
2023-07-20 13:53:47 +0200

    Add files via upload

---
+++ WelcomeHlungwani.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.welcomehlungwani;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author GOD IS GOOD
+ */
+public class WelcomeHlungwani extends JFrame {
+    
+    public WelcomeHlungwani()
+    {
+        setTitle("Hlungwani Welcome Ndzalama");
+        setSize(250, 350);
+        setDefaultLookAndFeelDecorated(true);
+        setForeground(Color.red);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(true);
+        setVisible(true);
+    }
+    
+}
---
+++ WelcomeHlungwaniApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package welcomehlungwaniapp;
+
+import za.ac.tut.welcomehlungwani.WelcomeHlungwani;
+
+/**
+ *
+ * @author GOD IS GOOD
+ */
+public class WelcomeHlungwaniApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declare
+        WelcomeHlungwani well;
+        
+        well = new WelcomeHlungwani();
+        
+        
+    }
+    
+}
